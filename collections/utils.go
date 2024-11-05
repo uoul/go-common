8172abe30db652e0d707ae95492f67ea63ddd186
Lukas Neuhauser <l.neuhauser@iotwin.at>
2024-11-05 13:56:06 +0100

    fixed impl in collection.utils

--- collections/utils.go
+++ collections/utils.go
@@ -1,9 +1,9 @@
 package collections
 
-func Map[T,V any](col []T, fn(T) V) []V {
-  r := make([]V, len(col))
-  for i, e := range col {
-      r[i] = fn(e)
-  }
-  return r
+func Map[T, V any](col []T, fn func(T) V) []V {
+	r := make([]V, len(col))
+	for i, e := range col {
+		r[i] = fn(e)
+	}
+	return r
 }
